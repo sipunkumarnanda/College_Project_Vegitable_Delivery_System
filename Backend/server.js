6cc676d4cd70c34a406b0aa0d99b21ef2211e9ab
Sipun Kumar Nanda <98945228+sipunkumarnanda@users.noreply.github.com>
2026-04-15 16:50:54 +0000

    update logic in db.js and server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,21 +1,11 @@
+
 import dotenv from 'dotenv';
-import mongoose from 'mongoose';
 import app from './src/app.js';
+import connectDB from './src/db/db.js';
 
-// Load environment variables
 dotenv.config();
+connectDB();
 
-const PORT = process.env.PORT || 5000;
-
-// Database Connection and Server Start
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log('Connected to MongoDB');
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
-    });
-  })
-  .catch((error) => {
-    console.error('Database connection error:', error.message);
-    process.exit(1);
-  });
+app.listen(3000, () => {
+    console.log('server is running on port 3000');
+});
---
+++ Backend/src/db/db.js
@@ -0,0 +1,14 @@
+
+import mongoose from 'mongoose';
+
+async function connectDB() {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log('Connected to MongoDB');
+    } catch (err) {
+        console.error('MongoDB connection error:', err);
+        process.exit(1);
+    }
+}
+
+export default connectDB;
